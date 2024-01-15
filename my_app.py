290402434b27880146013fdabd0935dd99cea75d
mwg-dmg <156708696+mwg-dmg@users.noreply.github.com>
2024-01-15 13:59:33 -0500

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import streamlit as st
+#print("hello")
+
+st.title(":blue[Test Data]")
+
+url="test_a_b.csv"
+df=pd.read_csv(url)
+#print(df)
+st.dataframe(df)
+st.write(df.shape[0])
+n=(df.shape[0]-1)
+st.write(n)
+
+
+
+
+
+
+
+
+
+
+
